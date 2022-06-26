dac182849c893d4ded89dbc49c2ece60654f9940
DESKTOP-B529Q6N\HHJ <zmfpdkep@naver.com>
2022-06-26 22:33:28 +0900

    #0626 문제풀이

---
+++ problem/05_FUNC/03_1065/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_1065
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                if (HanSu(i.ToString())) count++;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static bool HanSu(string num)
+        {
+
+            if (int.Parse(num) < 100) return true;
+
+            int[] numbers = new int[num.Length];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                numbers[i] = int.Parse(num[i].ToString());
+            }
+
+            if (numbers[0] - numbers[1] == numbers[1] - numbers[2])
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

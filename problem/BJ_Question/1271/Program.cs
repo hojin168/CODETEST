a42f03c537d25f07bdc0c58712789884cf44dfc2
DESKTOP-B529Q6N\HHJ <zmfpdkep@naver.com>
2022-06-29 08:20:17 +0900

    #0629 문제풀이

---
+++ problem/BJ_Question/1271/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1271
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s =  (Console.ReadLine()).Split(' ');
+            int[] ss = Array.ConvertAll(s, int.Parse);
+
+            Console.WriteLine(ss[0] / ss[1]);
+            Console.WriteLine(ss[0] % ss[1]);
+        }
+    }
+}

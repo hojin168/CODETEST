3df75fb49ec712f671fe35f5a9e21a186a5a7560
DESKTOP-B529Q6N\HHJ <zmfpdkep@naver.com>
2022-07-01 07:39:27 +0900

    #0701 문제풀이

---
+++ problem/BJ_Question/2743/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2743
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char [] strInput = Console.ReadLine().ToCharArray();
+            Console.WriteLine(strInput.GetLength(0));
+
+        }
+    }
+}

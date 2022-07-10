541393dec3db2551d7a090aa29d9a1939883384b
DESKTOP-B529Q6N\HHJ <zmfpdkep@naver.com>
2022-07-11 07:29:02 +0900

    #0711 문제풀이

---
+++ problem/BJ_Question/10807/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10807
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numN = int.Parse(Console.ReadLine());
+            string str1 = Console.ReadLine();
+            int numv = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            string[] arr1 = str1.Split(' ');
+            int[] arrInt = Array.ConvertAll(arr1, int.Parse);
+
+            for(int i = 0; i < numN; i++)
+            {
+                if (arrInt[i] == numv)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+        }
+    }
+}
---
+++ problem/BJ_Question/4999/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4999
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            int n1 = str1.Length;
+            int n2 = str2.Length;
+
+            if (n1 < n2)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine("go");
+            }
+
+        }
+    }
+}
